36aae729cd89df6e2174daa3da92f9ba08dda4fc
NGOC NGUYEN <thaingoc273@gmail.com>
2022-07-15 12:35:59 +0200

    correct slide

--- Ngoc_recommender_movie.py
+++ Ngoc_recommender_movie.py
@@ -34,7 +34,7 @@ def main():
         st.write('The mean of rating should be greater than **3**')
         st.write('The number of rating should be not less than **20**')
         st.markdown('# 3. User-user base recommender')
-        st.write('We use two similarity distances **cosine** and **Person correlation**')
+        st.write('We use two similarity distances **Cosine** and **Person correlation**')
         st.markdown('# 4. Singular values decomposition (SVD)')
         st.write('Decompose the large matrix (pivot table) of users and movies into the product of two small matricies')
         st.write('Choose **k=30** for low dimension matrices')        
